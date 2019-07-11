81148a91c4ba218d4621784961e9e03e1eebacde
Davide Nastri <d.nastri@gmail.com>
2019-07-11 22:30:41 +0200

    feat: Clean project, ready to start from scratch

--- src/main/java/document/Document.java
+++ src/main/java/document/Document.java
@@ -1,6 +1,6 @@
 package document;
 
-/** 
+/**
  * A class that represents a text document
  * @author UC San Diego Intermediate Programming MOOC team
  */
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public abstract class Document {
 
 	private String text;
-	
+
 	/** Create a new document from the given text.
 	 * Because this class is abstract, this is used only from subclasses.
 	 * @param text The text of the document.
@@ -21,7 +21,7 @@ public abstract class Document {
 	{
 		this.text = text;
 	}
-	
+
 	/** Returns the tokens that match the regex pattern from the document 
 	 * text string.
 	 * @param pattern A regular expression string specifying the 
@@ -34,27 +34,27 @@ public abstract class Document {
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-		
+
 		while (m.find()) {
 			tokens.add(m.group());
 		}
-		
+
 		return tokens;
 	}
-	
+
 	/** This is a helper function that returns the number of syllables
 	 * in a word.  You should write this and use it in your 
 	 * BasicDocument class.
-	 * 
+	 *
 	 * You will probably NOT need to add a countWords or a countSentences 
 	 * method here.  The reason we put countSyllables here because we'll 
 	 * use it again next week when we implement the EfficientDocument class.
-	 * 
+	 *
 	 * For reasons of efficiency you should not create Matcher or Pattern 
 	 * objects inside this method. Just use a loop to loop through the 
 	 * characters in the string and write your own logic for counting 
 	 * syllables.
-	 * 
+	 *
 	 * @param word  The word to count the syllables in
 	 * @return The number of syllables in the given word, according to 
 	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
@@ -65,13 +65,13 @@ public abstract class Document {
 	protected int countSyllables(String word)
 	{
 		// TODO: Implement this method so that you can call it from the 
-	    // getNumSyllables method in BasicDocument (module 2) and 
-	    // EfficientDocument (module 3).
-	    return 0;
+		// getNumSyllables method in BasicDocument (module 2) and
+		// EfficientDocument (module 3).
+		return 0;
 	}
-	
+
 	/** A method for testing
-	 * 
+	 *
 	 * @param doc The Document object to test
 	 * @param syllables The expected number of syllables
 	 * @param words The expected number of words
@@ -87,21 +87,21 @@ public abstract class Document {
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
 		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound
 					+ ", expected " + syllables);
 			passed = false;
 		}
 		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound
 					+ ", expected " + words);
 			passed = false;
 		}
 		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound
 					+ ", expected " + sentences);
 			passed = false;
 		}
-		
+
 		if (passed) {
 			System.out.println("passed.\n");
 		}
@@ -110,31 +110,31 @@ public abstract class Document {
 		}
 		return passed;
 	}
-	
-	
+
+
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-	
+
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-	
+
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-	
+
 	/** Return the entire text of this document */
 	public String getText()
 	{
 		return this.text;
 	}
-	
+
 	/** return the Flesch readability score of this document */
 	public double getFleschScore()
 	{
-	    // TODO: You will play with this method in week 1, and 
+		// TODO: You will play with this method in week 1, and
 		// then implement it in week 2
-	    return 0.0;
+		return 0.0;
 	}
-	
-	
-	
+
+
+
 }
