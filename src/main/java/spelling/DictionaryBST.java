f6c21e8d1f64d055fa0552f468f99ef27a323c42
Davide Nastri <d.nastri@gmail.com>
2019-08-14 00:44:43 +0200

    feat: Implemented Dictionary with Binary Search Tree

--- src/main/java/spelling/DictionaryBST.java
+++ src/main/java/spelling/DictionaryBST.java
@@ -9,18 +9,21 @@ import java.util.TreeSet;
 public class DictionaryBST implements Dictionary 
 {
    private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
-    
+
+    public DictionaryBST() {
+        dict = new TreeSet<String>();
+    }
+
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+        if (!isWord(word.toLowerCase())) {
+            dict.add(word.toLowerCase());
+            return true;
+        }
         return false;
     }
 
@@ -28,14 +31,12 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        return dict.contains(s.toLowerCase());
     }
 
 }
